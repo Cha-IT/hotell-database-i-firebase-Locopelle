27f934b75f9d979ed970b770f717a8cc6bb51d61
Locopelle <113931913+Locopelle@users.noreply.github.com>
2024-04-30 12:07:49 +0200

    done

--- getrooms.js
+++ getrooms.js
@@ -1,33 +1,71 @@
 import { db } from "./firebaseconfig.js"
 import { getFirestore, doc, addDoc, setDoc, getDoc, getDocs, query, where, orderBy, collection } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js"
 
-var allRooms = document.querySelector(".hotelRooms")
+var allRooms = document.querySelector(".hotelRooms");
 
-var selectedfloor = 1;
+var FloorFilter = document.querySelector("#FloorFilter")
+var BedsFilter = document.querySelector("#BedsFilter")
+var AvailableRoom = document.querySelector("#AvailableRoomFilter")
+
+var selectedfloor = "none";
 var selectedroomType = "none";
 var selectednoOfBeds = "none";
 var selectedisAvailable = "none";
 
+var i = 0;
+
+document.getElementById("ApplyFilters").addEventListener("click", function() {
+    selectedfloor = FloorFilter.value;
+    selectedroomType = TypeFilter.value;
+    selectednoOfBeds = BedsFilter.value;
+    selectedisAvailable = AvailableRoom.checked;
+    reloadRooms(allRooms)
+})
+
 async function getRooms() {
-    const rooms = await getDocs(query(collection(db, "room")));
+    const rooms = await getDocs(query(collection(db, "rooms")));
 
     rooms.forEach((room) => {
+        i++;
+
         if (
             (room.data().floor == selectedfloor || selectedfloor == "none") &&
             (room.data().roomType == selectedroomType || selectedroomType == "none") &&
             (room.data().noOfBeds == selectednoOfBeds || selectednoOfBeds == "none") &&
             (room.data().isAvailable == selectedisAvailable || selectedisAvailable == "none")
         ) {
-        var roomDiv = document.createElement("div");
-        roomDiv.id = room.data().room
-        allRooms.appendchild(roomDiv)
+            var roomDiv = document.createElement("div");
+            roomDiv.id = "room" +i;
+            roomDiv.className = "hotelRoomDiv"
+            allRooms.appendChild(roomDiv);
 
-        var roomFloor = document.createElement("p")
-        roomFloor.textContent = room.data().floor;
-        document.getElementsByID(roomDiv.id).appendchild(roomFloor);
+            var roomFloor = document.createElement("p");
+            roomFloor.textContent = "Etasje: " + room.data().floor;
+            document.getElementById(roomDiv.id).appendChild(roomFloor);
 
-        console.log(room.data());
+            var Type = document.createElement("p");
+            Type.textContent = "Rom type: " + room.data().roomType;
+            document.getElementById(roomDiv.id).appendChild(Type);
+
+            var roomBeds = document.createElement("p");
+            roomBeds.textContent = "Senge plasser: " + room.data().noOfBeds;
+            document.getElementById(roomDiv.id).appendChild(roomBeds);
+
+            var roomAvailable = document.createElement("p");
+            if (room.data().isAvailable == true) {roomAvailable.textContent = "Ledig"}
+            else {roomAvailable.textContent = "Opptatt"}
+            document.getElementById(roomDiv.id).appendChild(roomAvailable);
+
+            console.log(room.data());
         }
     });
+    i = 0;
 }
-getRooms()
+
+function reloadRooms(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+    getRooms()
+}
+getRooms();
